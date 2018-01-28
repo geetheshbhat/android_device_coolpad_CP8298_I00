ba61977bddb360ebed87d9efa910ea10f32d645a
Sougata Talukdar <sougatatalukdar77@gmail.com>
2018-01-28 14:48:45 +0530

    Fix Unofficial DotOS OTA

--- patches/install.sh
+++ patches/install.sh
@@ -1,6 +1,7 @@
 echo $1
+rm $rootdirectory/packages/apps/DotOTA/app/src/main/java/com/ota/updates/utils/Constants.java
 rootdirectory="$PWD"
-dirs="bionic frameworks/av frameworks/base frameworks/native system/core system/netd packages/apps/Settings hardware/libhardware"
+dirs="bionic frameworks/av frameworks/base frameworks/native system/core system/netd packages/apps/Settings hardware/libhardware packages/apps/DotOTA"
 for dir in $dirs ; do
 	cd $rootdirectory
 	cd $dir
